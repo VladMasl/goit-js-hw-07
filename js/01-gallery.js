8f1ea9e53b5c456bfe3e2ffc67d9906e275f5bc1
Vlad  Masliuk <102538039+VladMasl@users.noreply.github.com>
2022-06-27 10:38:33 +0300

    Mandatory part is ready to be checked!

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const blokListGallery = document.querySelector(".gallery");
+
+const imgCardSet = listGalleryItems(galleryItems);
+
+blokListGallery.insertAdjacentHTML("beforeend", imgCardSet);
+blokListGallery.addEventListener("click", onImgCardsClick);
+
+function listGalleryItems(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+  <img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+  />
+  </a>
+  </div>`
+    )
+    .join("");
+}
+
+function onImgCardsClick(evt) {
+  evt.preventDefault();
+  const isCardsSwatchEl = evt.target.classList.contains("gallery__image");
+  if (!isCardsSwatchEl) {
+    return;
+  }
+
+  const imagesCardSetEl = evt.target;
+
+  const modal = basicLightbox.create(`
+    <img src="${imagesCardSetEl.dataset.source}" width="800" height="600">`);
+  modal.show(() => {
+    window.addEventListener("keydown", (evt) => {
+      const isEscKey = evt.code === "Escape";
+
+      if (isEscKey) {
+        modal.close();
+      }
+    });
+  });
+}
+blokListGallery.addEventListener("click", onImgCardsClick);
