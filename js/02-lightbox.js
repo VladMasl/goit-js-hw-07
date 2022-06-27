91784b0ca3e950377e07f3d85d268817b0dddb1f
Vlad  Masliuk <102538039+VladMasl@users.noreply.github.com>
2022-06-27 15:12:51 +0300

    finishHw7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const blokListGallery = document.querySelector(".gallery");
+
+const imgCardSet = listGalleryItems(galleryItems);
+
+blokListGallery.insertAdjacentHTML("beforeend", imgCardSet);
+
+function listGalleryItems(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__link" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}"
+  alt="${description}"
+  />
+  </a>`
+    )
+    .join("");
+}
+
+new SimpleLightbox(".gallery__link ", {
+  captionsData: "alt",
+  captionDelay: "250",
+  enableKeyboard: "true",
+});
